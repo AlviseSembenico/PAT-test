e994985ab1fe2f6f12f7a56af2bcecd36f7b4353
Alvise <alvise.sembenico@studenti.unitn.it>
2019-08-05 15:47:26 +0200

    Table tools

--- pat_test/PAT/views.py
+++ pat_test/PAT/views.py
@@ -9,3 +9,9 @@ class ToolsList(ListView):
     template_name = 'tools_list.html'
     paginate_by = 10
     model = Tool
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for tool in context["object_list"]:
+            tool.pass_date = tool.revision_set.latest('date').date if tool.revision_set.count() > 0 else None
+        return context
