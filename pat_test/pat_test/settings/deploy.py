80ec5a55e72fc305f69c8a9e88bd7f6527db2f2a
Alvise <alvise.sembenico@studenti.unitn.it>
2019-08-12 17:16:57 +0200

    add db to docker-compose

--- pat_test/pat_test/settings/deploy.py
+++ pat_test/pat_test/settings/deploy.py
@@ -2,12 +2,12 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'admin'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'pwd_admin'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
     }
 }
-DEBUG = True
+DEBUG = False
