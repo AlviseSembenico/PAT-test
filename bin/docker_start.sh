1ae2bcc5c42603ea7e6c72202182839c5681427b
Alvise <alvise.sembenico@studenti.unitn.it>
2019-08-12 18:07:58 +0200

    remove psql check

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-db_host=${DB_HOST:-localhost}
-db_user=${DB_USER:-admin}
-db_password=${DB_PASSWORD:-pwd_admin}
-db_port=${DB_PORT:-5432}
-
-until PGPORT=$db_port PGPASSWORD=$db_password psql -h "$db_host" -U "$db_user" -c '\q'; do
-  >&2 echo "Waiting for database connection..."
-  sleep 1
-done
-
 echo "Apply database migrations"
 python pat_test/manage.py migrate
 
